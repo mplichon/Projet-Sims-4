d3970556ca70f0b2b3dc6593d9920659dc56d0a6
mplichon <plichon.matteo@gmail.com>
2025-10-30 11:27:43 +0100

    add mapping in Class TraitACondition

--- sims_back/src/main/java/sims/model/TraitACondition.java
+++ sims_back/src/main/java/sims/model/TraitACondition.java
@@ -1,17 +1,32 @@
 package sims.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+@Entity
 public class TraitACondition extends TraitDeCaractere {
 	
 	// Attributs
+	@Column(length = 50, nullable = false)
 	protected String condition;
 
 	
 	// Constructeurs
+	public TraitACondition() {}
+
+	
 	public TraitACondition(Integer id, String nom, String description, String img, TypeTrait type,
 			CategorieSim categorieSim, String condition) {
 		super(id, nom, description, img, type, categorieSim);
 		this.condition = condition;
 	}
+	
+	public TraitACondition(String nom, String description, String img, TypeTrait type,
+			CategorieSim categorieSim, String condition) {
+		super(nom, description, img, type, categorieSim);
+		this.condition = condition;
+	}
+
 
 
 	// Getters et Setters
