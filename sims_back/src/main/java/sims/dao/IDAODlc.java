9290dfcb7b89e12eb5e82d40bbb3fa6a90e4c9c1
mplichon <plichon.matteo@gmail.com>
2025-11-01 16:02:32 +0100

    remove an useless import

--- sims_back/src/main/java/sims/dao/IDAODlc.java
+++ sims_back/src/main/java/sims/dao/IDAODlc.java
@@ -3,7 +3,6 @@ package sims.dao;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import sims.model.DLC;
 import sims.model.TypeDLC;
