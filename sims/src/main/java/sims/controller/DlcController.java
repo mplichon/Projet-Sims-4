3e19f431a0671c4c4ceedfb9d0488f7dc1e26b0b
mplichon <plichon.matteo@gmail.com>
2025-11-09 23:25:34 +0100

    add redirectory and data processing for dlc (delete and add)

--- sims/src/main/java/sims/controller/DlcController.java
+++ sims/src/main/java/sims/controller/DlcController.java
@@ -4,8 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import sims.model.DLC;
+import sims.model.TypeDLC;
 import sims.service.DlcService;
 
 @Controller
@@ -18,6 +23,22 @@ public class DlcController {
 	@GetMapping
 	public String allDlc(Model model) {
 		model.addAttribute("dlcs", dlcSrv.getAll());
+		model.addAttribute("dlc", new DLC());
+		model.addAttribute("types", TypeDLC.values());
 		return "dlc/dlc";
+	}
+	
+	@PostMapping
+	public String createDlc(@ModelAttribute DLC dlc)
+	{
+		dlcSrv.create(dlc);
+		return "redirect:/dlc";
+	}
+	
+	@GetMapping("/delete/{id}")
+	public String deleteDlc(@PathVariable Integer id)
+	{ 
+		dlcSrv.deleteById(id);
+		return "redirect:/dlc";
 	}
 }
